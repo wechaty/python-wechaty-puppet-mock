7ccbf40db911dc72df09d91c72852c001e1ecca3
wjmcat <1435130236@qq.com>
2020-07-23 21:32:06 +0800

    add test sample code

---
+++ examples/room-mock.py
@@ -0,0 +1,10 @@
+"""room mock code"""
+
+
+def mock_for_room():
+    """simple code for room mocker"""
+    pass
+
+
+if __name__ == '__main__':
+    mock_for_room()
---
+++ tests/test_login.py
@@ -0,0 +1,10 @@
+"""login mock"""
+
+
+def login_mock_code():
+    """login mock"""
+    pass
+
+
+if __name__ == '__main__':
+    login_mock_code()
